3dbb6672ea7367a7c9f261387e9c0ee7f8c95a82
andrewdarvell <andrew@darvell.ru>
2014-08-08 15:58:09 +0400

    FirstCommit

---
+++ src/ru/darvell/englishwidget/EnglishWidget.java
@@ -0,0 +1,15 @@
+package ru.darvell.englishwidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class EnglishWidget extends Activity {
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
